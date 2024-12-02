bf601f2f6b0514c77deef8e25489238aca7ae4e5
emperorbona <138147819+emperorbona@users.noreply.github.com>
2024-12-02 20:28:39 +0100

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Home() {
     return (
         <div>
-            <h2>Welcome to the Air Rights Marketplace</h2>
+            <h2>Welcome to the SkyTrade Marketplace</h2>
             <div className="links">
                 <Link to="/listings">View Listings</Link> |{" "}
                 <Link to="/add-listing">Add a Listing</Link> |{" "}
